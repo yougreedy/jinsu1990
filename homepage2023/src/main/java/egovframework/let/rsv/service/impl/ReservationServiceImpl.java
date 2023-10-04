3543e30358615b1c173ed30423b01673b7b81a2f
Student <Student@Student-PC>
2023-10-04 14:21:14 +0900

    2023-10-04

---
+++ homepage2023/src/main/java/egovframework/let/rsv/service/impl/ReservationServiceImpl.java
@@ -0,0 +1,76 @@
+package egovframework.let.rsv.service.impl;
+
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Service;
+
+import egovframework.let.rsv.service.ReservationService;
+import egovframework.let.rsv.service.ReservationVO;
+import egovframework.rte.fdl.cmmn.EgovAbstractServiceImpl;
+import egovframework.rte.fdl.idgnr.EgovIdGnrService;
+import egovframework.rte.fdl.property.EgovPropertyService;
+import egovframework.rte.psl.dataaccess.util.EgovMap;
+
+
+@Service("reservationService")
+public class ReservationServiceImpl extends EgovAbstractServiceImpl implements ReservationService{
+
+	@Resource(name = "propertiesService")
+	protected EgovPropertyService propertyService;
+	
+	@Resource(name = "reservationMapper")
+	private ReservationMapper reservationMapper;
+	
+	@Resource(name = "egovRsvIdGnrService")
+	private EgovIdGnrService idgenService;
+	
+	
+	//예약 목록 가져오기
+	@Override
+	public List<EgovMap> selectReservationList(ReservationVO vo) throws Exception {
+		return reservationMapper. selectReservationList(vo);
+	}
+
+	//예약 목록 수
+	@Override
+	public int selectReservationListCnt(ReservationVO vo) throws Exception {
+		return reservationMapper.selectReservationListCnt(vo);
+	}
+	
+	//예약 상세정보
+	@Override
+	public ReservationVO selectReservation(ReservationVO vo) throws Exception {
+		return reservationMapper.selectReservation(vo);
+	}
+	
+	//예약 등록하기
+	@Override
+	public String insertReservation(ReservationVO vo) throws Exception {
+		String id = idgenService.getNextStringId();
+		vo.setResveId(id);
+	    reservationMapper.insertReservation(vo);
+	    
+	    return id;
+	}
+	
+	//예약 수정하기
+	@Override
+	public void updateReservation(ReservationVO vo) throws Exception {
+        reservationMapper.updateReservation(vo);
+	}
+	
+	//예약 삭제하기
+	@Override
+	public void deleteReservation(ReservationVO vo) throws Exception {
+        reservationMapper.deleteReservation	(vo);
+	}
+   
+ 
+  
+  
+  
+  
+		 
+}
